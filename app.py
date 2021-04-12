c1704994a17a6ada4cdfd270c84c76315a672ce3
GreenSpeed16 <creekside.jm@gmail.com>
2021-04-12 09:47:48 -0500

    Added index view function.

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'f;lkah3oiehg'
 
+@app.route('/')
+def index():
+    return '<h1>This page is functional</h1>'
+
 if __name__ == '__main__':
     app.run(debug=True)
